172dfcd0d008dac79343885c1bb49e971592e2b5
Home <erik-dougboy@hotmail.com>
2019-05-14 11:58:34 -0300

    run

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.myapplication.R.layout.activity_menuu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnTelaMenu = (Button) findViewById(R.id.btnTelaMenu);
+
+        btnTelaMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(activity_menuu);
+            }
+        });
+    }
+
+
+}
+
